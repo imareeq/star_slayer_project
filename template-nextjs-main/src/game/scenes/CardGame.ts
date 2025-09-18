ef3f744a2580e9bd526381b1ab8f3a8b2398411c
Briyan <z5463890@ad.unsw.edu.au>
2025-09-18 14:07:25 +1000

    added: Card Game Template - Todo: Fix resizing

--- template-nextjs-main/src/game/scenes/CardGame.ts
+++ template-nextjs-main/src/game/scenes/CardGame.ts
@@ -1,6 +1,16 @@
 import { GameObjects, Scene } from 'phaser';
 import { EventBus } from '../EventBus';
 
+import { createCard, CardObject } from "../createCard";
+
+interface GridConfiguration {
+    x: number;
+    y: number;
+    paddingX: number;
+    paddingY: number;
+}
+
+
 export class CardGame extends Scene {
     background: GameObjects.Image;
     private buttons: Phaser.GameObjects.Rectangle[] = [];
@@ -32,13 +42,54 @@ export class CardGame extends Scene {
     private pauseOverlay!: Phaser.GameObjects.Rectangle;
     private pauseMenu!: Phaser.GameObjects.Container;
     private isPaused: boolean = false;
+    // Cards Game Objects
+    private cardOpened?: CardObject;
+    private cards: CardObject[] = [];
+    private canMove: boolean = false;
+    private lives: number = 3; // Assuming a default value; adjust as needed
+    private cardNames: string[] = ["card-0", "card-1", "card-2", "card-3", "card-4", "card-5"]; // Populate with actual card names
+    private gridConfiguration: GridConfiguration = {
+        x: 0,
+        y: 0,
+        paddingX: 10,
+        paddingY: 10
+    };
+
+    init ()
+    {
+        this.cameras.main.fadeIn(500);
+        this.lives = 5;
+    }
 
     constructor() {
         super('CardGame');
     }
 
     preload() {
-        this.load.image('cardgame-bg', 'assets/card_game/card_lore_bg.png');
+        this.load.setPath("assets/card_game/");
+
+        // this.load.image("volume-icon", "ui/volume-icon.png");
+        // this.load.image("volume-icon_off", "ui/volume-icon_off.png");
+
+        // this.load.audio("theme-song", "audio/fat-caps-audionatix.mp3");
+        // this.load.audio("whoosh", "audio/whoosh.mp3");
+        // this.load.audio("card-flip", "audio/card-flip.mp3");
+        // this.load.audio("card-match", "audio/card-match.mp3");
+        // this.load.audio("card-mismatch", "audio/card-mismatch.mp3");
+        // this.load.audio("card-slide", "audio/card-slide.mp3");
+        // this.load.audio("victory", "audio/victory.mp3");
+        this.load.image('cardgame-bg', 'card_lore_bg.png');
+        this.load.image("minigame-background", "card_minigame_bg.png");
+        this.load.image("card-back", "card_unopened.png");
+        this.load.image("card-0", "./card_faces/face_1.png");
+        this.load.image("card-1", "./card_faces/face_2.png");
+        this.load.image("card-2", "./card_faces/face_3.png");
+        this.load.image("card-3", "./card_faces/face_4.png");
+        this.load.image("card-4", "./card_faces/face_5.png");
+        this.load.image("card-5", "./card_faces/face_6.png");
+        this.load.image("card-6", "./card_faces/face_7.png");
+        this.load.image("card-7", "./card_faces/face_8.png");
+        this.load.image("heart", "pixel_heart.png");
     }
 
     create() {
@@ -51,16 +102,16 @@ export class CardGame extends Scene {
 
         // Create focus indicator
         this.focusIndicator = this.add.rectangle(0, 0, 310, 65, 0xffff00, 0)
-            .setStrokeStyle(2, 0xffff00)
+        .setStrokeStyle(2, 0xffff00)
             .setVisible(false)
             .setDepth(10000);
 
         const canvas = this.game.canvas as HTMLCanvasElement;
         if (canvas) {
-            canvas.setAttribute('tabindex', '0');
-            canvas.focus();
+        canvas.setAttribute('tabindex', '0');
+        canvas.focus();
             this.canvasClickHandler = () => {
-                canvas.focus();
+            canvas.focus();
                 console.log('Canvas focused via click (CardGame)');
             };
             canvas.addEventListener('click', this.canvasClickHandler);
@@ -77,7 +128,7 @@ export class CardGame extends Scene {
 
         // Start cutscene
         this.startCutscene();
-
+        
         EventBus.emit('current-scene-ready', this);
     }
 
@@ -226,6 +277,7 @@ export class CardGame extends Scene {
         this.cutsceneText.destroy();
         this.input.off('pointerdown', this.advanceCutscene, this);
         // Game logic starts here
+        // this.startGame();
     }
 
     private layout = () => {
@@ -267,6 +319,286 @@ export class CardGame extends Scene {
             .setPosition(b.centerX, b.centerY)
             .setVisible(true);
     }
+
+    // restartGame ()
+    // {
+    //     this.cardOpened = undefined;
+    //     this.cameras.main.fadeOut(200 * this.cards.length);
+    //     this.cards.reverse().map((card, index) => {
+    //         this.add.tween({
+    //             targets: card.gameObject,
+    //             duration: 500,
+    //             y: 1000,
+    //             delay: index * 100,
+    //             onComplete: () => {
+    //                 card.gameObject.destroy();
+    //             }
+    //         })
+    //     });
+
+    //     this.time.addEvent({
+    //         delay: 200 * this.cards.length,
+    //         callback: () => {
+    //             this.cards = [];
+    //             this.canMove = false;
+    //             this.scene.restart();
+    //             // this.sound.play("card-slide", { volume: 1.2 });
+    //         }
+    //     })
+    // }
+
+    // createGridCards ()
+    // {
+    //     // Phaser random array position
+    //     const gridCardNames = Phaser.Utils.Array.Shuffle([...this.cardNames, ...this.cardNames]);
+
+    //     return gridCardNames.map((name, index) => {
+    //         const newCard = createCard({
+    //             scene: this,
+    //             x: this.gridConfiguration.x + (98 + this.gridConfiguration.paddingX) * (index % 4),
+    //             y: -1000,
+    //             frontTexture: name,
+    //             cardName: name
+    //         });
+    //         this.add.tween({
+    //             targets: newCard.gameObject,
+    //             duration: 800,
+    //             delay: index * 100,
+    //             // onStart: () => this.sound.play("card-slide", { volume: 1.2 }),
+    //             y: this.gridConfiguration.y + (128 + this.gridConfiguration.paddingY) * Math.floor(index / 4)
+    //         })
+    //         return newCard;
+    //     });
+    // }
+
+    // createHearts ()
+    // {
+    //     return Array.from(new Array(this.lives)).map((el, index) => {
+    //         const heart = this.add.image(this.sys.game.scale.width + 1000, 20, "heart")
+    //             .setScale(2)
+
+    //         this.add.tween({
+    //             targets: heart,
+    //             ease: Phaser.Math.Easing.Expo.InOut,
+    //             duration: 1000,
+    //             delay: 1000 + index * 200,
+    //             x: 140 + 30 * index // marginLeft + spaceBetween * index
+    //         });
+    //         return heart;
+    //     });
+    // }
+
+
+    // volumeButton ()
+    // {
+    //     // const volumeIcon = this.add.image(25, 25, "volume-icon").setName("volume-icon");
+    //     // volumeIcon.setInteractive();
+
+    //     // Mouse enter
+    //     // volumeIcon.on(Phaser.Input.Events.POINTER_OVER, () => {
+    //     //     this.input.setDefaultCursor("pointer");
+    //     // });
+    //     // // Mouse leave
+    //     // volumeIcon.on(Phaser.Input.Events.POINTER_OUT, () => {
+    //     //     console.log("Mouse leave");
+    //     //     this.input.setDefaultCursor("default");
+    //     // });
+
+
+    //     // volumeIcon.on(Phaser.Input.Events.POINTER_DOWN, () => {
+    //     //     if (this.sound.volume === 0) {
+    //     //         this.sound.setVolume(1);
+    //     //         volumeIcon.setTexture("volume-icon");
+    //     //         volumeIcon.setAlpha(1);
+    //     //     } else {
+    //     //         this.sound.setVolume(0);
+    //     //         volumeIcon.setTexture("volume-icon_off");
+    //     //         volumeIcon.setAlpha(.5)
+    //     //     }
+    //     // });
+    // }
+
+    // startGame ()
+    // {
+
+    //     // WinnerText and GameOverText
+    //     const winnerText = this.add.text(this.sys.game.scale.width / 2, -1000, "YOU WIN",
+    //         { align: "center", strokeThickness: 4, fontSize: 40, fontStyle: "bold", color: "#8c7ae6" }
+    //     ).setOrigin(.5)
+    //         .setDepth(3)
+    //         .setInteractive();
+
+    //     const gameOverText = this.add.text(this.sys.game.scale.width / 2, -1000,
+    //         "GAME OVER\nClick to restart",
+    //         { align: "center", strokeThickness: 4, fontSize: 40, fontStyle: "bold", color: "#ff0000" }
+    //     )
+    //         .setName("gameOverText")
+    //         .setDepth(3)
+    //         .setOrigin(.5)
+    //         .setInteractive();
+
+    //     // Start lifes images
+    //     const hearts = this.createHearts();
+
+    //     // Create a grid of cards
+    //     this.cards = this.createGridCards();
+
+    //     // Start canMove
+    //     this.time.addEvent({
+    //         delay: 200 * this.cards.length,
+    //         callback: () => {
+    //             this.canMove = true;
+    //         }
+    //     });
+
+    //     // Game Logic
+    //     this.input.on(Phaser.Input.Events.POINTER_MOVE, (pointer: Phaser.Input.Pointer) => {
+    //         if (this.canMove) {
+    //             const card = this.cards.find(card => card.gameObject.hasFaceAt(pointer.x, pointer.y));
+    //             if (card) {
+    //                 this.input.setDefaultCursor("pointer");
+    //             } else {
+    //                 this.input.setDefaultCursor("default");
+    //             }
+    //         }
+    //     });
+    //     this.input.on(Phaser.Input.Events.POINTER_DOWN, (pointer: Phaser.Input.Pointer) => {
+    //         if (this.canMove && this.cards.length) {
+    //             const card = this.cards.find(card => card.gameObject.hasFaceAt(pointer.x, pointer.y));
+
+    //             if (card) {
+    //                 this.canMove = false;
+
+    //                 // Detect if there is a card opened
+    //                 if (this.cardOpened !== undefined) {
+    //                     // If the card is the same that the opened not do anything
+    //                     if (this.cardOpened?.gameObject.x === card.gameObject.x && this.cardOpened?.gameObject.y === card.gameObject.y) {
+    //                         this.canMove = true;
+    //                         return false;
+    //                     }
+
+    //                     card.flip(() => {
+    //                         if (this.cardOpened?.cardName === card.cardName) {
+    //                             // ------- Match -------
+    //                             // this.sound.play("card-match");
+    //                             // Destroy card selected and card opened from history
+    //                             this.cardOpened?.destroy();
+    //                             card.destroy();
+
+    //                             // remove card destroyed from array
+    //                             this.cards = this.cards.filter(cardLocal => cardLocal.cardName !== card.cardName);
+    //                             // reset history card opened
+    //                             this.cardOpened = undefined;
+    //                             this.canMove = true;
+
+    //                         } else {
+    //                             // ------- No match -------
+    //                             // this.sound.play("card-mismatch");
+    //                             this.cameras.main.shake(600, 0.01);
+    //                             // remove life and heart
+    //                             const lastHeart = hearts[hearts.length - 1];
+    //                             this.add.tween({
+    //                                 targets: lastHeart,
+    //                                 ease: Phaser.Math.Easing.Expo.InOut,
+    //                                 duration: 1000,
+    //                                 y: - 1000,
+    //                                 onComplete: () => {
+    //                                     lastHeart.destroy();
+    //                                     hearts.pop();
+    //                                 }
+    //                             });
+    //                             this.lives -= 1;
+    //                             // Flip last card selected and flip the card opened from history and reset history
+    //                             card.flip();
+    //                             this.cardOpened?.flip(() => {
+    //                                 this.cardOpened = undefined;
+    //                                 this.canMove = true;
+
+    //                             });
+    //                         }
+
+    //                         // Check if the game is over
+    //                         if (this.lives === 0) {
+    //                             // Show Game Over text
+    //                             // this.sound.play("whoosh", { volume: 1.3 });
+    //                             this.add.tween({
+    //                                 targets: gameOverText,
+    //                                 ease: Phaser.Math.Easing.Bounce.Out,
+    //                                 y: this.sys.game.scale.height / 2,
+    //                             });
+
+    //                             this.canMove = false;
+    //                         }
+
+    //                         // Check if the game is won
+    //                         if (this.cards.length === 0) {
+    //                             // this.sound.play("whoosh", { volume: 1.3 });
+    //                             // this.sound.play("victory");
+
+    //                             this.add.tween({
+    //                                 targets: winnerText,
+    //                                 ease: Phaser.Math.Easing.Bounce.Out,
+    //                                 y: this.sys.game.scale.height / 2,
+    //                             });
+    //                             this.canMove = false;
+    //                         }
+    //                     });
+
+    //                 } else if (this.cardOpened === undefined && this.lives > 0 && this.cards.length > 0) {
+    //                     // If there is not a card opened save the card selected
+    //                     card.flip(() => {
+    //                         this.canMove = true;
+    //                     });
+    //                     this.cardOpened = card;
+    //                 }
+    //             }
+    //         }
+
+    //     });
+
+
+    //     // Text events
+    //     winnerText.on(Phaser.Input.Events.POINTER_OVER, () => {
+    //         winnerText.setColor("#FF7F50");
+    //         this.input.setDefaultCursor("pointer");
+    //     });
+    //     winnerText.on(Phaser.Input.Events.POINTER_OUT, () => {
+    //         winnerText.setColor("#8c7ae6");
+    //         this.input.setDefaultCursor("default");
+    //     });
+    //     winnerText.on(Phaser.Input.Events.POINTER_DOWN, () => {
+    //         // this.sound.play("whoosh", { volume: 1.3 });
+    //         this.add.tween({
+    //             targets: winnerText,
+    //             ease: Phaser.Math.Easing.Bounce.InOut,
+    //             y: -1000,
+    //             onComplete: () => {
+    //                 this.restartGame();
+    //             }
+    //         })
+    //     });
+
+    //     gameOverText.on(Phaser.Input.Events.POINTER_OVER, () => {
+    //         gameOverText.setColor("#FF7F50");
+    //         this.input.setDefaultCursor("pointer");
+    //     });
+
+    //     gameOverText.on(Phaser.Input.Events.POINTER_OUT, () => {
+    //         gameOverText.setColor("#8c7ae6");
+    //         this.input.setDefaultCursor("default");
+    //     });
+
+    //     gameOverText.on(Phaser.Input.Events.POINTER_DOWN, () => {
+    //         this.add.tween({
+    //             targets: gameOverText,
+    //             ease: Phaser.Math.Easing.Bounce.InOut,
+    //             y: -1000,
+    //             onComplete: () => {
+    //                 this.restartGame();
+    //             }
+    //         })
+    //     });
+    // }
 
     private cleanup() {
         const canvas = this.game.canvas as HTMLCanvasElement;
